c4d92a15694ab6931a861aefc9d24893590af802
John Wallace <John.Wallace@noaa.gov>
2022-02-14 12:14:06 -0800

    Changed 'del_args_Makeconf' argument name
    to 'del_Makeconf_flags'

--- R/Compile.R
+++ R/Compile.R
@@ -1,5 +1,5 @@
 
-Compile <- function(file, flags = "", ..., del_args_Makeconf = "", verbose = FALSE) { 
+Compile <- function(file, flags = "", ..., del_Makeconf_flags = "", verbose = FALSE) { 
  
     Makeconf_file <- paste0(R.home("etc"), Sys.getenv("R_ARCH"), "/Makeconf")
     if(verbose) cat("\nGlobal Makeconf file being used:",  Makeconf_file, "\n")
@@ -7,8 +7,8 @@ Compile <- function(file, flags = "", ..., del_args_Makeconf = "", verbose = FAL
         Makeconf <- scan(Makeconf_file, what = "", sep = "\n", quiet = TRUE)
         Makeconf_args_global <- sub(" \\$\\(LTO\\)", "", sub(" \\$\\(DEBUGFLAG\\) ", " ", sub("CXXFLAGS = ", "", Makeconf[grep("^CXXFLAGS", Makeconf)])))
         if(verbose) cat("\nCXXFLAGS retreived:", Makeconf_args_global, "\n")
-        if(length(del_args_Makeconf) > 1 || del_args_Makeconf != "") {
-           for(i in del_args_Makeconf)
+        if(length(del_Makeconf_flags) > 1 || del_Makeconf_flags != "") {
+           for(i in del_Makeconf_flags)
               Makeconf_args_global <- sub(i, "", Makeconf_args_global)
         }
      } else
@@ -16,4 +16,4 @@ Compile <- function(file, flags = "", ..., del_args_Makeconf = "", verbose = FAL
      flags <- paste(Makeconf_args_global, "-Wno-ignored-attributes", paste(flags[flags!=""], collapse = " ")) ## Now flags overrides only the Makeconf cxxflags that are the same. [Need paste here, not 'c()'.]
      if(verbose) cat("\nRemaining CXXFLAGS plus flags added:", flags, "\n\n")
      TMB::compile(file = file, flags = flags, ...) 
-}     
+}               
